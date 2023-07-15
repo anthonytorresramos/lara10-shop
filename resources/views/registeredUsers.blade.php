74304cabb1a4d33738d18c03c98aff600ae9fe1b
anthony ramos <anthonytorresramos3@gmail.com>
2023-07-16 00:12:53 +0800

    > Enhance buttons on registeredUsers View

--- resources/views/registeredUsers.blade.php
+++ resources/views/registeredUsers.blade.php
@@ -13,7 +13,7 @@
 
 
                 <!-- User Table Begin -->
-                <div class="container my-12 py-12 mx-auto px-4 md:px-6 lg:px-12">
+                {{-- <div class="container my-12 py-12 mx-auto px-4 md:px-6 lg:px-12"> --}}
 
                     <!--Section: Design Block-->
                     <section class="mb-20 text-gray-800">
@@ -23,6 +23,25 @@
                                 <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                                     <div class="inline-block min-w-full sm:px-6 lg:px-8">
                                         <div class="overflow-hidden">
+
+                                            <button type="button" title="View All Deleted"
+                                                class="my-4 float-right text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                                                </svg>
+                                            </button>
+
+                                            <button type="button" title="Create New User"
+                                                class="my-4 float-right text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                                                </svg>
+                                            </button>
+
                                             <table class="min-w-full mb-0">
                                                 <thead class="border-b bg-gray-50 rounded-t-lg text-left">
                                                     <tr>
@@ -91,7 +110,8 @@
                                                             class="align-middle text-right text-sm font-normal px-6 py-4 whitespace-nowrap border-black">
                                                             <div class="flex items-center justify-content-end">
                                                                 <a href="#"
-                                                                    class="text-green-400 hover:text-green-700 hover:scale-110 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out">
+                                                                    class="text-green-400 hover:text-green-700 hover:scale-110 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out"
+                                                                    title="details">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                                         viewBox="0 0 24 24" stroke-width="1.5"
                                                                         stroke="currentColor" class="w-6 h-6">
@@ -105,7 +125,8 @@
 
                                                                 </a>
                                                                 <a href="#"
-                                                                    class="text-green-400 hover:text-green-700 hover:scale-110 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out">
+                                                                    class="text-green-400 hover:text-green-700 hover:scale-110 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out"
+                                                                    title="edit">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                                         viewBox="0 0 24 24" stroke-width="1.5"
                                                                         stroke="currentColor" class="w-6 h-6">
@@ -116,7 +137,8 @@
 
                                                                 </a>
                                                                 <a href="#"
-                                                                    class="text-red-400 hover:text-red-700 hover:scale-110 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out">
+                                                                    class="text-red-400 hover:text-red-700 hover:scale-110 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out"
+                                                                    title="delete">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                                         viewBox="0 0 24 24" stroke-width="1.5"
                                                                         stroke="currentColor" class="w-6 h-6">
@@ -145,7 +167,7 @@
 
 
 
-                                            <div class="d-flex justify-content-center m-3">
+                                            <div class="my-4 d-flex justify-content-center m-3">
                                                 <span class="inline-flex items-center">
                                                     <span class="text-gray-500">Showing {{ $users->perPage() }}
                                                         entries</span>
@@ -196,7 +218,8 @@
                     </section>
                     <!--Section: Design Block-->
 
-                </div>
+                    {{--
+                </div> --}}
                 <!-- User Table End -->
 
 

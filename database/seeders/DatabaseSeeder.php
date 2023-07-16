f076ff26d94e2fefa83ba8a501d0979f96c07dac
anthony ramos <anthonytorresramos3@gmail.com>
2023-07-17 06:03:54 +0800

    # Add ProductFactory.php # modify DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        \App\Models\Product::factory(50)->create();
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'anthony ramos',
+            'email' => 'anthonytorresramos@gmail.com',
+            'password' => Hash::make(1234),
+        ]);
     }
 }
